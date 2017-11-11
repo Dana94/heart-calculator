2227594b90c803618f69c399c6b7aaae3391d9ff
Dana <dana.ottaviani@gmail.com>
2017-11-11 18:19:32 -0500

    added second button row, and assigned colors

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 var buttons = document.getElementsByClassName('btn');
 
 
-for(var i = 0; i < buttons.length; i++){
-    console.log(buttons[i].id);
-    buttons[i].addEventListener('click', () => {
-        console.log(buttons[i].id);
-    });
-}
+// for(var i = 0; i < buttons.length; i++){
+//     console.log(buttons[i].id);
+//     buttons[i].addEventListener('click', () => {
+//         console.log(buttons[i].id);
+//     });
+// }
