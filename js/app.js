a23898cbe95b28eced04c39218e71a964132021f
Dana <dana.ottaviani@gmail.com>
2018-02-04 14:41:12 -0500

    figuring out best way to add event listeners and calculate total

--- js/app.js
+++ js/app.js
@@ -1,9 +1,33 @@
 var buttons = document.getElementsByClassName('btn');
+//console.log(buttons);
+
+for(var i = 0; i < buttons.length; i++){
+    //console.log(buttons[i].id);
+    buttons[i].addEventListener('click', () => {
+        var id = this.id;
+        console.log(id);
+    });
+}
 
 
-// for(var i = 0; i < buttons.length; i++){
-//     console.log(buttons[i].id);
-//     buttons[i].addEventListener('click', () => {
-//         console.log(buttons[i].id);
-//     });
-// }
+document.getElementById('zero').addEventListener('click', () => {
+        console.log(0);
+});
+
+document.getElementById('one').addEventListener('click', () => {
+        console.log(0);
+});
+
+document.getElementById('add').addEventListener('click', () => {
+        console.log(0);
+});
+
+document.getElementById('equals').addEventListener('click', () => {
+        calculate();
+});
+
+function calculate() {
+
+
+    
+}
